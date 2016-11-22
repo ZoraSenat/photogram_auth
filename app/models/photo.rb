38e870f067281308efe6e306f70b03c42593d766
zora <ssh>
2016-11-22 11:07:55 -0600

    Added usernames to photo comments

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,7 +6,7 @@ class Photo < ActiveRecord::Base
   # associations
   has_many :comments
 
-  belongs_to :users
+  belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
 
   has_many :likes
 
